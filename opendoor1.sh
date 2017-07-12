a76cca29fed10309590d5b7b52a697be5d83fb14
Enzotech2 <enzotech2.techno@gmail.com>
2017-07-12 17:31:02 +1200

    Added Opendoor

---
+++ opendoor1.sh
@@ -0,0 +1,9 @@
+echo "You open the door..."
+sleep 1
+echo "An angry bear jumps out at you"
+sleep 2
+echo "But as soon as it sees the torch it runs of down the smelly corrider."
+sleep 1
+echo "You think to yourself..."
+echo "Why am I here? How did I get here?"
+sleep 3
