b99dd1542a79552b2098a596db157d6ef4a4a7fe
Enzotech2 <enzotech2.techno@gmail.com>
2017-07-12 17:11:36 +1200

    Add files via upload

---
+++ START-GAME.sh
@@ -0,0 +1,20 @@
+echo "NEED SUDO PERMS..."
+sudo -v
+echo "Hello, Welcome to a game."
+sleep 2
+echo "You have to choices on each qestion."
+sleep 1
+echo "Are you ready?"
+echo "Hope you are 'cause im about to start..."
+sleep 5
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+./op1.sh
