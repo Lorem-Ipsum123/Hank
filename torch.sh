a975763379267e9cd9e3bfd23d5f3fc630bdf2c7
Enzotech2 <enzotech2.techno@gmail.com>
2017-07-12 17:22:52 +1200

    Fix torch.sh yes/no Qestion

--- torch.sh
+++ torch.sh
@@ -4,9 +4,9 @@ sleep 3
 echo "What would you like to do?"
 echo " "
 sleep 3
-read -p "Would you like to open the door? [Y/n]" dt
+read -p "Would you like to open the door? [D/t]" dt
     case $yn in
-        [Yy]* ) ./opendoor1.sh
-        [Nn]* ) echo "Well you have to there is nothing else left to do :("
+        [Dd]* ) ./opendoor1.sh
+        [Tt]* ) echo "Well you have to there is nothing else left to do :("
         * ) echo "Please answer D or T.";;
     esac
